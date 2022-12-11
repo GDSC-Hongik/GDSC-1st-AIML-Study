dada3c758b930ca4e446988e23df61de6d771bbc
paragonyun <paragonyun@naver.com>
2022-12-11 13:56:30 +0900

    [Add] Add V3 Version

--- Dacon/dataloader.py
+++ Dacon/dataloader.py
@@ -1,5 +1,5 @@
 from torch.utils.data import DataLoader
-from dataset import GDSCDataset, GDSCDatasetV2
+from dataset import GDSCDataset, GDSCDatasetV2, GDSCDatasetV3
 from sklearn.model_selection import train_test_split
 import cv2
 import pandas as pd
@@ -8,7 +8,7 @@ import torch
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
-def return_dataloaders(df : pd.DataFrame, V2=False) -> torch.utils.data.DataLoader :
+def return_dataloaders(df : pd.DataFrame, ver='2') -> torch.utils.data.DataLoader :
 
     train_df, val_df, train_labels, val_labels = train_test_split(
                                                         df.drop(columns=['N_category']), 
@@ -17,19 +17,26 @@ def return_dataloaders(df : pd.DataFrame, V2=False) -> torch.utils.data.DataLoad
                                                         random_state=42
                                                     )
 
-    if V2 == False :
+    if ver == '1' :
         train_dataset = GDSCDataset(medical_df=train_df, labels=train_labels.values, train_mode=True)
         train_loader = DataLoader(train_dataset, batch_size=1)
 
         val_dataset = GDSCDataset(medical_df=val_df, labels=val_labels.values, train_mode=False)
         val_loader = DataLoader(val_dataset, batch_size=1)
 
-    elif V2 == True :
+    elif ver == '2' :
         train_dataset = GDSCDatasetV2(medical_df=train_df, labels=train_labels.values, train_mode=True)
         train_loader = DataLoader(train_dataset, batch_size=16)
 
         val_dataset = GDSCDatasetV2(medical_df=val_df, labels=val_labels.values, train_mode=False)
         val_loader = DataLoader(val_dataset, batch_size=16)
+
+    elif ver =='3' :
+        train_dataset = GDSCDatasetV3(medical_df=train_df, labels=train_labels.values, train_mode=True)
+        train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+
+        val_dataset = GDSCDatasetV3(medical_df=train_df, labels=val_labels.values, train_mode=False)
+        val_loader = DataLoader(val_dataset, batch_size=1)
 
     print(f'✅ # of Train Datas : {len(train_dataset)}')
     print(f'✅ # of Validation Datas : {len(val_dataset)}')
