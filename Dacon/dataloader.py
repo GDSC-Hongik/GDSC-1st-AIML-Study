c55d8521e99b89577294055a11fff4df85e376a8
paragonyun <paragonyun@naver.com>
2022-12-23 14:31:11 +0900

    [Add] Test Loader

--- Dacon/dataloader.py
+++ Dacon/dataloader.py
@@ -10,19 +10,19 @@ from albumentations.pytorch.transforms import ToTensorV2
 
 from prep import Prep
 
-def return_dataloaders(df : pd.DataFrame, ver='2') -> torch.utils.data.DataLoader :
+def return_dataloaders(tr_ori_df : pd.DataFrame, test_df: pd.DataFrame, ver='2') -> torch.utils.data.DataLoader :
 
 
     train_df, val_df, train_labels, val_labels = train_test_split(
-                                                        df.drop(columns=['N_category']), 
-                                                        df['N_category'], 
+                                                        tr_ori_df.drop(columns=['N_category']), 
+                                                        tr_ori_df['N_category'], 
                                                         test_size=0.2, 
                                                         random_state=42
                                                     )
 
     ## Scaling
-    preper = Prep(train_df=train_df, val_df=val_df)
-    scaled_tr, scaled_val = preper.run()
+    preper = Prep(train_df=train_df, val_df=val_df, test_df=test_df)
+    scaled_tr, scaled_val, scaled_test = preper.run()
 
     if ver == '1' :
         train_dataset = GDSCDataset(medical_df=scaled_tr, labels=train_labels.values, train_mode=True)
@@ -45,7 +45,10 @@ def return_dataloaders(df : pd.DataFrame, ver='2') -> torch.utils.data.DataLoade
         val_dataset = GDSCDatasetV3(medical_df=scaled_val, labels=val_labels.values, train_mode=False)
         val_loader = DataLoader(val_dataset, batch_size=32)
 
+        test_dataset = GDSCDatasetV3(medical_df=scaled_test, labels=None, train_mode=False)
+        test_loader = DataLoader(test_dataset, batch_size=32)
+
     print(f'✅ # of Train Datas : {len(train_dataset)}')
     print(f'✅ # of Validation Datas : {len(val_dataset)}')
 
-    return train_loader, val_loader
+    return train_loader, val_loader, test_loader
